01494fe17d55b1fe11507b3990998557dceadc0a
hugowangguowei <hugowangguowei@gmail.com>
2014-12-08 00:01:06 +0800

    Revert "Revert "fundation""
    This reverts commit eb79eb374e899d5a62f4754748264e6f2196706c.

---
+++ 拼版效果/地图层上升/mouse_event.js
@@ -0,0 +1,45 @@
+﻿function getPointOnCanvas(canvas, x, y) {
+			var bbox = canvas.getBoundingClientRect();
+			return { x: x - bbox.left * (canvas.width  / bbox.width),
+					y: y - bbox.top  * (canvas.height / bbox.height)
+					};
+		}
+		
+function doMouseDown(event) {
+	var x = event.pageX;
+	var y = event.pageY;
+	var canvas = event.target;
+	var loc = getPointOnCanvas(canvas, x, y);
+	console.log("mouse down at point( x:" + loc.x + ", y:" + loc.y + ")");
+	context.beginPath();
+	context.clearRect(0,0,canvas.width,canvas.height);
+	context.fillRect(300,10,50,50);
+	context.closePath();
+	context.beginPath();
+	context.fillStyle = "#0000F1";
+	context.fillRect(loc.x-25,loc.y - 25,50,50);
+	//context.moveTo(loc.x, loc.y);
+	started = true;
+}
+		
+function doMouseMove(event) {
+	var x = event.pageX;
+	var y = event.pageY;
+	var canvas = event.target;
+	var loc = getPointOnCanvas(canvas, x, y);
+	if (started) {
+		//context.lineTo(loc.x, loc.y);
+		//context.stroke();
+		context.clearRect(0,0,canvas.width,canvas.height);
+		context.fillRect(300,10,50,50);
+		context.fillRect(loc.x-25,loc.y - 25,50,50);
+	}
+}
+		
+function doMouseUp(event) {
+	console.log("mouse up now");
+    if (started) {
+    	doMouseMove(event);
+        started = false;
+	}
+}
---
+++ 拼版效果/头像拼版绘制/mouse_event.js
@@ -0,0 +1,37 @@
+﻿function getPointOnCanvas(canvas, x, y) {
+			var bbox = canvas.getBoundingClientRect();
+			return { x: x - bbox.left * (canvas.width  / bbox.width),
+					y: y - bbox.top  * (canvas.height / bbox.height)
+					};
+		}
+		
+function doMouseDown(event) {
+	var x = event.pageX;
+	var y = event.pageY;
+	var canvas = document.getElementById("base_canvas");
+	var loc = getPointOnCanvas(canvas, x, y);
+	console.log("mouse down at point( x:" + loc.x + ", y:" + loc.y + ")");
+	draw_character(1,loc);
+	//context.moveTo(loc.x, loc.y);
+	started = true;
+}
+		
+function doMouseMove(event) {
+	var x = event.pageX;
+	var y = event.pageY;
+	var canvas = document.getElementById("event_canvas");
+	var context = canvas.getContext("2d");
+	var loc = getPointOnCanvas(canvas, x, y);
+	if (started) {
+		context.clearRect(0,0,1000,500);
+		draw_character(1,loc);
+	}
+}
+		
+function doMouseUp(event) {
+	console.log("mouse up now");
+    if (started) {
+    	doMouseMove(event);
+        started = false;
+	}
+}
