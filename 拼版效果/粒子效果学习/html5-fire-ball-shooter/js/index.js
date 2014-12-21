2de155ccc3291912de3b45735e92781e6d5f3f5a
hugowangguowei <hugowangguowei@gmail.com>
2014-12-22 02:35:33 +0800

    火焰射击效果学习
    主要用了一个array。。。

--- 拼版效果/粒子效果学习/html5-fire-ball-shooter/js/index.js
+++ 拼版效果/粒子效果学习/html5-fire-ball-shooter/js/index.js
@@ -1,13 +1,15 @@
-bgColor = '#111';
-gravity = 0.03;
+bgColor = '#000';
+gravity = 0.1;
 particleColor = '#f73';
 
 canvas = document.querySelector('canvas');
 ctx = canvas.getContext('2d');
 
-(onresize = function(){
+(onresize = function(){//当调整窗口大小时候调用该函数
   width = canvas.width = canvas.clientWidth;
   height = canvas.height = canvas.clientHeight;
+  //Math.floor为求一个浮点数的整数部分
+  //onresize的目的是自动转换坐标系？改成屏幕中心为坐标原点
   o = {x:Math.floor(width/2),y:Math.floor(height/2)};
   edge = {top:-o.y,right:width-o.x,bottom:height-o.y,left:-o.x}
 })();
@@ -57,12 +59,14 @@ onmousemove = function(e){
 
 charging = false;
 pos1 = {x:0,y:0};
-showInstructions = true;
+showInstructions = true;//展示示例
 onmousedown = function(e){
   pos1.x = mouse.x;
   pos1.y = mouse.y;
   charging = true;
   showInstructions = false;
+  console.log("x坐标为：" + x);
+  console.log("y坐标为：" + y);
 };
 
 onmouseup = function(){
@@ -81,16 +85,19 @@ onmouseup = function(){
 time = 0;
 requestAnimationFrame(loop = function(){
   ctx.setTransform(1,0,0,1,0,0);
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.globalAlpha = 1;
+  ctx.globalCompositeOperation = 'source-over';//图像覆盖方式：新图像会覆盖旧图片
+  ctx.globalAlpha = 1;//透明度为不透明
   ctx.fillStyle = bgColor;
   ctx.fillRect(0,0,width,height);
-  
+  //重设屏幕中心点的坐标
   ctx.translate(o.x,o.y);
   
   if(charging){
     var c = Math.floor(30+mouse.d/2);
+	console.log("C的值为：" + c);
+
     ctx.strokeStyle = 'rgba('+c+','+c+','+c+',1)';
+	console.log("strokeStyle的值为：" + ctx.strokeStyle);
     ctx.lineWidth = 4;
     ctx.beginPath();
     ctx.moveTo(pos1.x,pos1.y);
@@ -103,6 +110,7 @@ requestAnimationFrame(loop = function(){
     pos1.x = -70;
     pos1.y = -35;
     
+	//绘制目标汇聚的圆
     if(time<10){
       var x = -70,
           y = -35,
@@ -124,11 +132,13 @@ requestAnimationFrame(loop = function(){
           r = 30,
           a = 0;
     }
+	
+	//绘制引导线
     var dx = pos1.x-x,
         dy = pos1.y-y,
         d = Math.sqrt(dx*dx+dy*dy);
-    if(time<80&&time>10){
-      ctx.globalCompositeOperation = 'source-over';
+    if(time<80&&time>10){//只在10-80之间绘制
+      ctx.globalCompositeOperation = 'source-over';//后者覆盖前者
       ctx.globalAlpha = 1;
       var c = Math.floor(30+d/2);
       ctx.strokeStyle = 'rgba('+c+','+c+','+c+',1)';
@@ -164,7 +174,7 @@ requestAnimationFrame(loop = function(){
   for(var i in particles){
     var p = particles[i];
     ctx.beginPath();
-    ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
+    ctx.arc(p.x,p.y,p.r,0,Math.PI*2);//实际上只是画圆
     ctx.globalAlpha = p.o;
     ctx.fillStyle = p.c;
     ctx.fill();
@@ -182,7 +192,7 @@ requestAnimationFrame(loop = function(){
   
   for(var i in fireballs){
     f = fireballs[i];
-    var numParticles = Math.sqrt(f.xv*f.xv+f.yv*f.yv)/5;
+    var numParticles = Math.sqrt(f.xv*f.xv+f.yv*f.yv)/5;//粒子数目与速度有关，速度越快，粒子数越多，成正比
     if(numParticles<1)numParticles=1;
     var numParticlesInt = Math.ceil(numParticles),
         numParticlesDif = numParticles/numParticlesInt;
